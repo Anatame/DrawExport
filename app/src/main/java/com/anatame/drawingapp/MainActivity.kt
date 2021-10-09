d5167e8a82f1dd6f084367624d9ac13550af79bc
Anatame <anata.me97@gmail.com>
2021-10-09 15:40:16 +0530

    Added brush size chooser dialog

--- app/src/main/java/com/anatame/drawingapp/MainActivity.kt
+++ app/src/main/java/com/anatame/drawingapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.anatame.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         dvDrawingView.setSizeForBrush(20.toFloat())
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size: ")
+
+        val smallBtn = brushDialog.ib_small_brush
+        val mediumBtn = brushDialog.ib_medium_brush
+        val largeBtn = brushDialog.ib_large_brush
+
+        smallBtn.setOnClickListener{
+            dvDrawingView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        mediumBtn.setOnClickListener{
+            dvDrawingView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        largeBtn.setOnClickListener{
+            dvDrawingView.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
