2070de20542f49ae9398fe86a1d6366217d2c8b6
Anatame <anata.me97@gmail.com>
2021-10-09 14:34:14 +0530

    Added persistence of lines on the view, shape for background, margin for the view

--- app/src/main/java/com/anatame/drawingapp/DrawingView.kt
+++ app/src/main/java/com/anatame/drawingapp/DrawingView.kt
@@ -15,6 +15,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
@@ -44,6 +45,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
 
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path!!.brushThickness
+            mDrawPaint!!.color = path!!.color
+            canvas.drawPath(path!!, mDrawPaint!!)
+        }
+
         if(!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -72,6 +79,8 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
             }
 
             MotionEvent.ACTION_UP -> {
+
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
 
