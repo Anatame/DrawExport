585fa18f18ee9d9e8688e2fb09517868081f291b
Anatame <anata.me97@gmail.com>
2021-10-09 14:50:19 +0530

    Added set brush thickness from main method with consideration of screen dimensions.

--- app/src/main/java/com/anatame/drawingapp/DrawingView.kt
+++ app/src/main/java/com/anatame/drawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.anatame.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import java.util.jar.Attributes
@@ -30,7 +31,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+       // mBrushSize = 20.toFloat()
 
     }
 
@@ -92,6 +93,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     }
 
 
+    fun setSizeForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics)
+
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
 
 
     internal inner class CustomPath
